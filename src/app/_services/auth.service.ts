1c25b10753cb63d4c37e645e0e0efd85544fc6cb
chunyim <chunhinyim@gmail.com>
2023-10-20 19:08:51 -0400

    httpoptions amended

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -5,7 +5,6 @@
 // •	register(): POST {username, email, password}
 // •	logout(): POST logout request
 
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -13,19 +12,21 @@ import { Observable } from 'rxjs';
 // const AUTH_API = 'http://localhost:8080/api/auth/';
 const AUTH_API = 'https://s-5099.onrender.com/api/auth/';
 
-
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+  }),
 };
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthService {
-  constructor(private http: HttpClient) { }
- 
+  constructor(private http: HttpClient) {}
+
   login(username: string, password: string): Observable<any> {
     return this.http.post(
       AUTH_API + 'signin',
@@ -36,7 +37,7 @@ export class AuthService {
       httpOptions
     );
   }
- 
+
   register(username: string, email: string, password: string): Observable<any> {
     return this.http.post(
       AUTH_API + 'signup',
@@ -48,7 +49,7 @@ export class AuthService {
       httpOptions
     );
   }
- 
+
   logout(): Observable<any> {
     return this.http.post(AUTH_API + 'signout', {}, httpOptions);
   }
